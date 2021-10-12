958e9e738551d035d65a92d7eb4ae1269cc3de5b
gedonik <gedonik@bk.ru>
2021-10-12 10:28:36 +0300

    first page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
-});
+var swiper = new Swiper('.realise__swiper', {
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'progressbar',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
